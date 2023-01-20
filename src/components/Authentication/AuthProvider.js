140fcf0f3f1de43a003a5ee0d75d95db861781b8
Mohd <azeemsre1@gmail.com>
2023-01-20 21:55:48 +0530

    verifyUserEmail

--- src/components/Authentication/AuthProvider.js
+++ src/components/Authentication/AuthProvider.js
@@ -4,16 +4,27 @@ import React, { useState } from 'react'
 import AuthContext from './auth-context';
 
 const AuthProvider = (props) => {
-    const [userIsLogin,setuserIsLogin]=useState(false)
+    const initialLogin=localStorage.getItem('idToken');
+    const [userIsLogin,setuserIsLogin]=useState(initialLogin)
 
 
     const loginHandler =()=>{
         setuserIsLogin(true);
     }
+    const logoutHandler=()=>{
+        localStorage.removeItem('idToken');
+        setuserIsLogin(false);
+    };
+
+    const verificationHandler=()=>{
+
+    }
 
     const authValues={
         isLogin: userIsLogin ,
         login:loginHandler,
+        logout:logoutHandler,
+        verification:verificationHandler
     }
   return (
     <AuthContext.Provider value={authValues}>
--- src/components/Pages/WelcomeScreen.js
+++ src/components/Pages/WelcomeScreen.js
@@ -1,8 +1,35 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useContext } from 'react'
 import { Link } from 'react-router-dom'
+import AuthContext from '../Authentication/auth-context'
 import classes from './WelcomeScreen.module.css'
 
 const WelcomeScreen = () => {
+
+    const ctx=useContext(AuthContext)
+
+    const url='https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyAzlQHFRtkaZpExFfx1mBDR64QU8JL9mO4'
+
+    const verifyEmailHandler=(e)=>{
+        e.preventDefault();
+        fetch(url,{
+            method:'POST',
+            body:JSON.stringify({
+                requestType:"VERIFY_EMAIL",
+                idToken:localStorage.getItem('idToken')
+            }),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }).then((res)=>{
+            const data=res.json();
+            data.then((resp)=>{
+                console.log(resp);
+            })
+        }).catch((err)=>{
+            console.log('err',err)
+        })
+    }
+
   return (
     <Fragment>
         <div className={classes.main}>
@@ -13,6 +40,8 @@ const WelcomeScreen = () => {
                 Your profile is incomplete.<Link to='/completeprofile'>Complete now</Link>
             </div>
         </div>
+        <button type='submit' onClick={verifyEmailHandler} className={classes.verifyEmail}>Verify Email</button>
+        <button className={classes.logout} onClick={()=>ctx.logout()}>logout</button>
     </Fragment>
   )
 }
