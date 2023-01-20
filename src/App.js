5ecd7b41e8e305113aa7a0baf7c531609a622697
Mohd <azeemsre1@gmail.com>
2023-01-20 15:55:42 +0530

    completeYourProfile

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import Authentication from './components/Authentication/Authentication';
-import AuthProvider from './components/Authentication/AuthProvider';
-import { useContext } from 'react';
+
+import { Fragment, useContext } from 'react';
 import AuthContext from './components/Authentication/auth-context';
 import WelcomeScreen from './components/Pages/WelcomeScreen';
+import { Route, Routes } from 'react-router-dom';
+import CompleteProfile from './components/Pages/CompleteProfile/CompleteProfile';
 
 function App() {
 
   const ctx =useContext(AuthContext)
 
   return (
-    <AuthProvider>
-    <div className="App">
-      {ctx.isLogin ? <Authentication /> : <WelcomeScreen />}
-    </div>
-    </AuthProvider>
+    
+    <Fragment>
+      <Routes>
+        <Route path='/' element= {!ctx.isLogin ? <Authentication /> : <WelcomeScreen />} />
+        <Route path='/completeprofile' element={!ctx.isLogin ? <Authentication /> : <CompleteProfile />} />
+
+
+      </Routes>
+     
+      </Fragment>
   );
 }
 
--- src/components/Pages/WelcomeScreen.js
+++ src/components/Pages/WelcomeScreen.js
@@ -1,9 +1,18 @@
 import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
+import classes from './WelcomeScreen.module.css'
 
 const WelcomeScreen = () => {
   return (
     <Fragment>
-        <h1>Welcome to expance tracker</h1>
+        <div className={classes.main}>
+            <div className={classes.left}>
+            Welcome to expance tracker!!!!
+            </div>
+            <div className={classes.right}>
+                Your profile is incomplete.<Link to='/completeprofile'>Complete now</Link>
+            </div>
+        </div>
     </Fragment>
   )
 }
