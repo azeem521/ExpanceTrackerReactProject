3190f51c6dcece955fdd4f1398c7b4e94ff8de3f
Mohd <azeemsre1@gmail.com>
2023-01-22 17:41:21 +0530

    editingAndDeletingExpenses

--- src/components/Pages/Expense/ExpenseItems.js
+++ src/components/Pages/Expense/ExpenseItems.js
@@ -1,13 +1,18 @@
 import React, { Fragment, useContext, useEffect, useState } from 'react'
 import { StoreData } from '../../storeOfData/Store';
+import classes from './ExpenseItems.module.css'
 
 const ExpenseItems = () => {
 
     const [amount,setAmount]=useState();
     const [catagory,setCatagory]=useState('Movie');
     const [discription,setDiscription]=useState();
-    const [totalItem,settotalItem]=useState()
+    const [id,setid]=useState(null);
+    const [isEditing,setisEditing]=useState(false);
+    const [showExp,setshowExp]=useState(false)
     const ctx=useContext(StoreData);
+
+     const [reRender,setreRender]=useState(true)
 
     const amountChangeHandler=(e)=>{
         setAmount(e.target.value)
@@ -19,10 +24,15 @@ const ExpenseItems = () => {
         setDiscription(e.target.value)
     }
 
+    const showExpenseHandler=()=>{
+      setshowExp((prev)=>!prev);
+  }
+
     const url='https://expancetrackerauth-default-rtdb.firebaseio.com/';
+    const email=localStorage.getItem('email')
 
     const getDataFrom=async ()=>{
-        const email=localStorage.getItem('email')
+        
         const response= await fetch(`${url}${email}.json`,{
           method:'GET'
       });
@@ -40,10 +50,38 @@ const ExpenseItems = () => {
     }
 
 
+    
+
+    const editHandler =(id,amount1,catagory1,descriptio1)=>{
+      setisEditing(true);
+      setid(id)
+      setAmount(amount1);
+      setCatagory(catagory1);
+      setDiscription(descriptio1);
+
+
+    }
+
+
+    const toDeleteData=async(id)=>{
+      const resp=await fetch(`${url}${email}/${id}.json`,{
+        method:'DELETE',
+        headers:{
+          'Content-Type':'application/json'
+      }
+      });
+      const respo=await resp.json();
+      setreRender((prev)=>!prev)
+      console.log('respo',respo,id);
+    };
+
+
     const submitHandler= async(e)=>{
-        e.preventDefault();
-        const email=localStorage.getItem('email')
-       console.log('ctx.items',ctx.items);
+      e.preventDefault();
+     console.log('ctx.items',ctx.items);
+
+     if(!isEditing){
+
        const response = await fetch(`${url}${email}.json`,{
         method:'POST',
         body:JSON.stringify({
@@ -57,11 +95,54 @@ const ExpenseItems = () => {
        });
        const data1=await response.json();
        console.log('data222',data1);
-       getDataFrom();
-    }
+     }
+
+     else{
+      const res=await fetch(`${url}${email}/${id}.json`,{
+        method:'PUT',
+        body:JSON.stringify({
+          amount:amount,
+          catagory:catagory,
+          decription:discription
+        }),
+        headers:{
+            'Content-Type':'application/json'
+        }
+       });
+       setisEditing(false);
+     }
+     getDataFrom();
+     setAmount('')
+       setCatagory('')
+       setDiscription('')
+       
+  }
+
+
+    useEffect(() => {
+      async function fetchMyAPI() {
+        let response = await fetch(`${url}${email}.json`,{
+          method:'GET'
+      })
+        const data = await response.json()
+        const newItem=[];
+        for(let key in data){
+          newItem.push({id:key,...data[key]})
+        }
+        console.log('useEffectCalled',newItem);
+        ctx.addItem(newItem);
+      }
+  
+      fetchMyAPI()
+    }, [reRender])
+
 
   return (
     <Fragment>
+      <div className={classes.addExp}>
+        <button type="button" className="btn btn-secondary" onClick={showExpenseHandler}>{!showExp ? '+Add Expense' : 'Close'}</button>
+        </div>
+     { showExp && <div className={classes.form}>
         <div>
         <div>
   <label className="form-label">Choose Expense</label>
@@ -87,9 +168,45 @@ const ExpenseItems = () => {
   <label  className="form-label">Add Short Description</label>
     <input type="text" className="form-control" placeholder="Description" aria-label="Last name" onChange={discriptionChangeHandler} value={discription}/>
   </div>
-  <button className="btn btn-primary mt-4 btn1" type="submit" onClick={submitHandler}>Submit</button>
+  <button className="btn btn-primary mt-4 btn1" type="submit" onClick={submitHandler}>{!isEditing ? 'Submit' : 'Update'}</button>
  
   </div>
+
+  </div> 
+}
+
+  <table className="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Expense</th>
+        <th scope="col">Catagory</th>
+        <th scope="col">Description</th>
+        <th scope="col">Edit</th>
+        <th scope="col">Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+   
+{
+    ctx.items.map((item,indx)=>(
+        <tr key={item.id}>
+        <th scope="row">{indx+1}</th>
+        <td>{item.amount}</td>
+        <td>{item.catagory}</td>
+        <td>{item.decription}</td>
+        <td><button type='button' className='btn btn-warning' onClick={editHandler.bind(null,item.id,item.amount,item.catagory,item.decription)} >Edit</button></td>
+        <td><button type='button' className='btn btn-danger' onClick={toDeleteData.bind(null,item.id)}>Delete</button></td>
+      </tr>
+    ))
+}
+
+
+      </tbody>
+    </table>
+
+
+
     </Fragment>
   )
 }
--- src/components/Pages/WelcomeScreen.js
+++ src/components/Pages/WelcomeScreen.js
@@ -3,18 +3,15 @@ import { Link } from 'react-router-dom'
 import AuthContext from '../Authentication/auth-context'
 import Store, { StoreData } from '../storeOfData/Store'
 import ExpenseItems from './Expense/ExpenseItems'
-import ExpenseTable from './Expense/ExpenseTable'
 import classes from './WelcomeScreen.module.css'
 
 const WelcomeScreen = () => {
-    const [showExp,setshowExp]=useState(false)
+    
 
     const authCtx=useContext(AuthContext)
     const ctx=useContext(StoreData);
 
-    const showExpenseHandler=()=>{
-        setshowExp((prev)=>!prev);
-    }
+    
 
     const url='https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyAzlQHFRtkaZpExFfx1mBDR64QU8JL9mO4'
 
@@ -58,14 +55,11 @@ const WelcomeScreen = () => {
         <button type='submit' onClick={verifyEmailHandler} className={classes.verifyEmail}>Verify Email</button>
         </div>
         <div className={classes.line}></div>
-        <div className={classes.addExp}>
-        <button type="button" className="btn btn-secondary" onClick={showExpenseHandler}>{!showExp ? '+Add Expense' : 'Close'}</button>
-        </div>
-      {showExp &&  <div className={classes.form}>
+        
+      <div className={classes.form}>
         <ExpenseItems />
-        </div>}
+        </div>
         <div className={classes.table}>
-        <ExpenseTable />
         </div>
       
      
